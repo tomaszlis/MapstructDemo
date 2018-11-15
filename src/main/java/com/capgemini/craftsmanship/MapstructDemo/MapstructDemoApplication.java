13ad32ca995c3776dc9f6a734712522cdb0ab268
Lis <tomasz.lis@capgemini.com>
2018-11-16 00:49:03 +0100

    Initial project setup

---
+++ src/main/java/com/capgemini/craftsmanship/MapstructDemo/MapstructDemoApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.craftsmanship.MapstructDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructDemoApplication.class, args);
+	}
+}
